b22272221d47f994e81e51504bd3004eb62b718d
Julian Schlarb <js@project-deadline.com>
2020-09-09 14:39:21 +0200

    bugfixing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+echo "Starting ... "
+
 if [[ -z "${CUSTOMER_IP_ADDRESS_ANNOTATION+x}" ]]; then
     echo "Variable CUSTOMER_IP_ADDRESS_ANNOTATION is empty or not defined" >&2
     exit 1
@@ -15,23 +17,23 @@ if [[ -z "${NODE_IP}" ]]; then
     exit 1
 fi
 
-
-if ip a | grep -q ${NODE_IP}/${NODE_IP_MASK}; then
-  echo "skipping"
+if  ip -o a show lo | cut -d ' ' -f 7 | grep -q ${NODE_IP}/${NODE_IP_MASK}; then
+  echo "${NODE_IP}/${NODE_IP_MASK} is already assigned to loopback ... skipping"
 else
   set -x
   ip a add ${NODE_IP}/${NODE_IP_MASK} dev lo
   { set +x; } 2>/dev/null
 fi
 
-if iptables -C POSTROUTING -s ${POD_SUBNET} -j SNAT --to-source ${NODE_IP} ; then
+if iptables -C POSTROUTING -s ${POD_SUBNET} ! -d ${POD_SUBNET} -j SNAT --to-source ${NODE_IP} > /dev/null 2>&1; then
   echo "rule already present ... skipping"
 else
   set -x
-  iptables -t nat -I POSTROUTING 1 -s ${POD_SUBNET} -j SNAT --to-source ${NODE_IP}
+  iptables -t nat -I POSTROUTING 1 -s ${POD_SUBNET} ! -d ${POD_SUBNET} -j SNAT --to-source ${NODE_IP}
   { set +x; } 2>/dev/null
 fi
 
+echo "Done"
 
 # suppress warnings
 pause >/dev/null 2>&1
